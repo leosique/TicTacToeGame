cf96084b301870b97a1d8bd9c32f17b65c3df7e6
leosique <lokinhokkonopacki@gmail.com>
2022-04-18 11:09:11 -0300

    adicionando a tabela

--- TicTacToe/TicTacToe/Form4.Designer.cs
+++ TicTacToe/TicTacToe/Form4.Designer.cs
@@ -29,6 +29,19 @@
         private void InitializeComponent()
         {
             this.fechar = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl9 = new System.Windows.Forms.Label();
+            this.lbl7 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.lbl6 = new System.Windows.Forms.Label();
+            this.lbl8 = new System.Windows.Forms.Label();
+            this.lbl5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.fechar)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,12 +57,157 @@
             this.fechar.TabStop = false;
             this.fechar.Click += new System.EventHandler(this.fechar_Click);
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(213, 147);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(350, 13);
+            this.panel1.TabIndex = 12;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Location = new System.Drawing.Point(213, 285);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(350, 13);
+            this.panel2.TabIndex = 13;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Location = new System.Drawing.Point(307, 50);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(13, 350);
+            this.panel3.TabIndex = 13;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.White;
+            this.panel4.Location = new System.Drawing.Point(454, 50);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(13, 350);
+            this.panel4.TabIndex = 14;
+            // 
+            // lbl1
+            // 
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.ForeColor = System.Drawing.Color.White;
+            this.lbl1.Location = new System.Drawing.Point(213, 50);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(88, 94);
+            this.lbl1.TabIndex = 15;
+            this.lbl1.Text = "O";
+            this.lbl1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl1.Click += new System.EventHandler(this.lbl1_Click);
+            // 
+            // lbl2
+            // 
+            this.lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.ForeColor = System.Drawing.Color.White;
+            this.lbl2.Location = new System.Drawing.Point(326, 50);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(122, 94);
+            this.lbl2.TabIndex = 16;
+            this.lbl2.Text = "O";
+            this.lbl2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl3
+            // 
+            this.lbl3.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl3.ForeColor = System.Drawing.Color.White;
+            this.lbl3.Location = new System.Drawing.Point(473, 50);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(90, 94);
+            this.lbl3.TabIndex = 17;
+            this.lbl3.Text = "X";
+            this.lbl3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl9
+            // 
+            this.lbl9.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl9.ForeColor = System.Drawing.Color.White;
+            this.lbl9.Location = new System.Drawing.Point(473, 301);
+            this.lbl9.Name = "lbl9";
+            this.lbl9.Size = new System.Drawing.Size(90, 99);
+            this.lbl9.TabIndex = 18;
+            this.lbl9.Text = "O";
+            this.lbl9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl7
+            // 
+            this.lbl7.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl7.ForeColor = System.Drawing.Color.White;
+            this.lbl7.Location = new System.Drawing.Point(213, 301);
+            this.lbl7.Name = "lbl7";
+            this.lbl7.Size = new System.Drawing.Size(88, 99);
+            this.lbl7.TabIndex = 19;
+            this.lbl7.Text = "X";
+            this.lbl7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl4
+            // 
+            this.lbl4.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl4.ForeColor = System.Drawing.Color.White;
+            this.lbl4.Location = new System.Drawing.Point(213, 163);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(88, 119);
+            this.lbl4.TabIndex = 20;
+            this.lbl4.Text = "X";
+            this.lbl4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl6
+            // 
+            this.lbl6.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl6.ForeColor = System.Drawing.Color.White;
+            this.lbl6.Location = new System.Drawing.Point(473, 163);
+            this.lbl6.Name = "lbl6";
+            this.lbl6.Size = new System.Drawing.Size(90, 119);
+            this.lbl6.TabIndex = 21;
+            this.lbl6.Text = "X";
+            this.lbl6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl8
+            // 
+            this.lbl8.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl8.ForeColor = System.Drawing.Color.White;
+            this.lbl8.Location = new System.Drawing.Point(326, 301);
+            this.lbl8.Name = "lbl8";
+            this.lbl8.Size = new System.Drawing.Size(122, 99);
+            this.lbl8.TabIndex = 22;
+            this.lbl8.Text = "O";
+            this.lbl8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl5
+            // 
+            this.lbl5.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl5.ForeColor = System.Drawing.Color.White;
+            this.lbl5.Location = new System.Drawing.Point(326, 163);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(122, 119);
+            this.lbl5.TabIndex = 23;
+            this.lbl5.Text = "X";
+            this.lbl5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkSeaGreen;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl5);
+            this.Controls.Add(this.lbl8);
+            this.Controls.Add(this.lbl6);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.lbl7);
+            this.Controls.Add(this.lbl9);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.fechar);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form4";
@@ -63,5 +221,18 @@
         #endregion
 
         private System.Windows.Forms.PictureBox fechar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl9;
+        private System.Windows.Forms.Label lbl7;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Label lbl6;
+        private System.Windows.Forms.Label lbl8;
+        private System.Windows.Forms.Label lbl5;
     }
 }
--- TicTacToe/TicTacToe/Form4.cs
+++ TicTacToe/TicTacToe/Form4.cs
@@ -12,6 +12,7 @@ namespace TicTacToe
 {
     public partial class Form4 : Form
     {
+        int cont = 0;
         public Form4()
         {
             InitializeComponent();
@@ -20,6 +21,21 @@ namespace TicTacToe
         private void fechar_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+        private void lbl1_Click(object sender, EventArgs e)
+        {
+            if(cont == 0)
+            {
+                lbl1.Text = "X";
+                cont = 1;
+            }
+            else if(cont == 1)
+            {
+                lbl1.Text = "O";
+                cont = 0;
+            }
+
         }
     }
 }
